a9ece379ede3a6ff8255d03170ef7830787ac8ab
tetragrade <186331795+tetragrade@users.noreply.github.com>
2025-07-19 21:58:42 +0100

    moved styling up to prevent flash on load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,6 @@
     :global(h1, h2) {
         text-align: center;
     }
-    :global(:root) {
-        font-family: system-ui, Avenir, Helvetica, Arial, sans-serif;
-        line-height: 1.5;
-        font-weight: 400;
-
-        color-scheme: light dark;
-        color: rgba(255, 255, 255, 0.87);
-        background-color: #242424;
-
-        font-synthesis: none;
-        text-rendering: optimizeLegibility;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
 
     :global(p) {
         margin: 0;
